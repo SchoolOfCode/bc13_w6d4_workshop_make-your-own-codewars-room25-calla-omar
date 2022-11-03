42024bcb2e4fe6fda986b2e9c3e9087ce7254410
Callasaurus <c.sullivandrage@outlook.com>
2022-11-03 14:30:33 +0000

    kata created

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 /* 
 👉 Write your kata here!
+
+You're making your weekly trip to Tesco for a food shop! We're in the year 3008 and now machines
+are running the world and they can only understand numbers... You've created a shopping list which 
+consists of numbers which represent specific food items. 
+
+You're trying to be healthy so you decide you only want to buy the fruit that you've put on your 
+shopping list. All fruitsitems are represented by a number ending with a 3. Return a new
+shopping list with only the fruit items. 
+
+Examples: 
+
+[136453, 353543, 763543, 875654, 121, 373, 2, 14, 3] => [136453, 353543, 763543, 373, 3]
+[16745, 37, 863, 233, 1, 22562, 753, 986432, 173] => [863, 233, 753, 173]
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export default function returnAllFruit(foodshop) {
+// Good luck! 
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,35 @@
 //👉 Write your tests below here:
+
+import returnAllFruit from "./main";
+import {expect, test} from "@jest/globals"
+
+test("Checking if all fruits are returned in an array", function () {
+    let actual = returnAllFruit([123, 456, 323, 678, 986, 333]);
+    let expected = [123, 323,333];
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checking if all fruits are returned in an array", function () {
+    let actual = returnAllFruit([123543, 4, 323563, 67, 9844, 3]);
+    let expected = [123543, 323563, 3];
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checking if all fruits are returned in an array", function () {
+    let actual = returnAllFruit([36, 35625, 2714, 6718, 9, 63]);
+    let expected = [63];
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checking if all fruits are returned in an array", function () {
+    let actual = returnAllFruit([125, 433, 764, 833, 983, 333]);
+    let expected = [433, 833, 983, 333];
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checking if a list with no fruit returns an empty array", function () {
+    let actual = returnAllFruit([122, 456, 322, 678, 986, 332]);
+    let expected = [];
+    expect(actual).toStrictEqual(expected)
+});
+
